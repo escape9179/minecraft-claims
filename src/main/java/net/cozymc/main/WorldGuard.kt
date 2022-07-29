8bddb98c95de47fdb14f999b68f29e4ecdc8cabd
escape9179 <tr3.logan@gmail.com>
2022-07-29 13:00:21 -0400

    Add region check

--- src/main/java/net/cozymc/main/WorldGuard.kt
+++ src/main/java/net/cozymc/main/WorldGuard.kt
@@ -1,8 +1,16 @@
 package net.cozymc.main
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.math.BlockVector2
+import com.sk89q.worldguard.WorldGuard
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin
 import org.bukkit.Location
 
 object WorldGuard {
 
-    fun isRegion(location: Location): Boolean = TODO("Implementation")
+    fun isRegion(location: Location): Boolean {
+        return WorldGuard.getInstance().platform.regionContainer.get(BukkitAdapter.adapt(location.world))?.regions?.any {
+            it.value.contains(BlockVector2.at(location.x, location.z))
+        } ?: false
+    }
 }
