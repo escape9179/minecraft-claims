17ab017c05a14ee49c87a76fb76354d3772413f4
escape9179 <tr3.logan@gmail.com>
2022-08-01 00:24:59 -0400

    Remove unneeded money parameter

--- src/main/java/net/cozymc/main/file/MainConfig.kt
+++ src/main/java/net/cozymc/main/file/MainConfig.kt
@@ -18,8 +18,8 @@ object MainConfig {
         return config.getString("message.chunkClaimSuccess")
     }
 
-    fun getChunkClaimFailureMoneyMessage(money: Int): String {
-        return String.format(config.getString("message.chunkClaimFailureMoney"), money)
+    fun getChunkClaimFailureMoneyMessage(): String {
+        return String.format(config.getString("message.chunkClaimFailureMoney"), getClaimCost())
     }
 
     fun getChunkClaimFailureClaimedMessage(): String {
