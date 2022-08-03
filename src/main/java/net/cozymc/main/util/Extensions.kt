d15c9e7a0e18138bd29af998e253dfea84a78c76
Tre Logan <tr3.logan@gmail.com>
2022-08-03 15:42:06 -0400

    Add claim particle effects (#1)
    * Register serializable Claim class
    
    * Add PlayerThread object
    
    * Add PlayerThread object
    
    * Refactoring
    
    * Make forEachChunk take 'it' receiver
    
    * Add prototype chunk particle effects

--- src/main/java/net/cozymc/main/util/Extensions.kt
+++ src/main/java/net/cozymc/main/util/Extensions.kt
@@ -1,9 +1,47 @@
 package net.cozymc.main.util
 
-import com.earth2me.essentials.Essentials
+import net.cozymc.main.Claim
 import net.cozymc.main.CozyClaimsPlugin
+import net.cozymc.main.chunk.Adjacency
+import net.cozymc.main.chunk.AdjacentChunk
+import net.cozymc.main.file.DataConfig
+import org.bukkit.Chunk
+import org.bukkit.Particle
 import org.bukkit.entity.Player
+import java.util.*
 
 fun Player.getBalance(): Int {
     return CozyClaimsPlugin.essentials.getUser(this).money.intValueExact()
+}
+
+fun Player.isInOwnClaim(): Boolean {
+    return DataConfig.getClaim(uniqueId)?.chunks?.any { location.chunk == it } ?: false
+}
+
+fun Player.getClaim(): Claim? {
+    return DataConfig.getClaim(uniqueId)
+}
+
+fun Player.playParticlesAroundClaim(claim: Claim) {
+    claim.forEachChunk { chunk ->
+        chunk.getAdjacentChunks().forEach { adjacentChunk ->
+            if (!adjacentChunk.chunk.isClaimOf(uniqueId)) {
+                adjacentChunk.borderBlocks.filter { it.y > location.y - 10 && it.y < location.y + 10 }
+                    .forEach { spawnParticle(Particle.VILLAGER_HAPPY, it.location, 3, 0.0, 0.0, 0.0) }
+            }
+        }
+    }
+}
+
+fun Chunk.getAdjacentChunks(): List<AdjacentChunk> {
+    return listOf(
+        AdjacentChunk(world.getChunkAt(x + 1, z), Adjacency.POS_X),
+        AdjacentChunk(world.getChunkAt(x, z + 1), Adjacency.POS_Z),
+        AdjacentChunk(world.getChunkAt(x - 1, z), Adjacency.NEG_X),
+        AdjacentChunk(world.getChunkAt(x, z - 1), Adjacency.NEG_Z),
+    )
+}
+
+fun Chunk.isClaimOf(uuid: UUID): Boolean {
+    return DataConfig.getClaim(uuid)?.chunks?.contains(this) ?: false
 }
