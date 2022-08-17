6ebb60ecefbff9762ea5a0d11a751e5ead77d2d6
escape9179 <tr3.logan@gmail.com>
2022-08-16 23:58:30 -0400

    Implement 'distrust' command

--- src/main/java/net/cozymc/main/command/DistrustCommand.kt
+++ src/main/java/net/cozymc/main/command/DistrustCommand.kt
@@ -2,6 +2,10 @@ package net.cozymc.main.command
 
 import logan.api.command.BasicCommand
 import logan.api.command.SenderTarget
+import net.cozymc.main.file.DataConfig
+import net.cozymc.main.file.MainConfig
+import net.cozymc.main.util.isClaimOwner
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 class DistrustCommand : BasicCommand<Player>(
@@ -9,6 +13,22 @@ class DistrustCommand : BasicCommand<Player>(
     1..1, target = SenderTarget.PLAYER, parentCommand = "claim"
 ) {
     override fun run(sender: Player, args: Array<out String>, data: Any?): Boolean {
-        TODO("Not yet implemented")
+        val trustee = Bukkit.getPlayer(args[0]) ?: kotlin.run {
+            sender.sendMessage(MainConfig.getUnknownPlayerMessage())
+            return true
+        }
+
+        if (!sender.isClaimOwner()) {
+            sender.sendMessage(MainConfig.getNotClaimOwnerMessage())
+            return true
+        }
+
+        val claim = DataConfig.loadClaim(sender.uniqueId)
+        if (claim!!.removeTrustee(trustee)) {
+            sender.sendMessage(MainConfig.getRemoveTrusteeSuccessMessage(trustee.name))
+            DataConfig.saveClaim(claim)
+        } else sender.sendMessage(MainConfig.getNotTrusteeOfClaimMessage())
+
+        return true
     }
 }
